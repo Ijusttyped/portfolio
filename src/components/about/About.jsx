c9188e2d9c907961a9fc572173c3a1209ef96b0c
Marcel Fernandez Rosas <60543555+Ijusttyped@users.noreply.github.com>
2022-04-06 11:12:13 +0200

    Customized header

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./about.css"
-import ME from "../../assets/me-about.jpg"
+import ME from "../../assets/me-about.png"
 import {FaAward} from "react-icons/fa"
 import {FiUsers} from "react-icons/fi"
 import {VscFolderLibrary} from "react-icons/vsc"
@@ -23,19 +23,19 @@ const About = () => {
                         <article className={"about__card"}>
                             <FaAward className={"about__icon"}/>
                             <h5>Experience</h5>
-                            <small>3+ Years Working Experience</small>
+                            <small>3+ Years Working</small>
                         </article>
 
                         <article className={"about__card"}>
                             <FiUsers className={"about__icon"}/>
                             <h5>Clients</h5>
-                            <small>Several</small>
+                            <small>3 Enterprise-Sized</small>
                         </article>
 
                         <article className={"about__card"}>
                             <VscFolderLibrary className={"about__icon"}/>
                             <h5>Projects</h5>
-                            <small>Some Completed Projects</small>
+                            <small>10+ Completed Projects</small>
                         </article>
                     </div>
                     <p>
