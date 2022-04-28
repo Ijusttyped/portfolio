ae7dfa346bf3fb72637b421b56b9da3d18fb04d6
Marcel Fernandez Rosas <60543555+Ijusttyped@users.noreply.github.com>
2022-04-28 15:32:32 +0200

    Added alt prop to images

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -55,7 +55,7 @@ const Testimonials = () => {
                         return (
                             <SwiperSlide key={id} className={"testimonial"}>
                                 <div className={"client__avatar"}>
-                                    <img src={avatar} />
+                                    <img src={avatar} alt={""}/>
                                 </div>
                                 <h5 className={"client__name"}>{name}</h5>
                                 <small className={"client__review"}>{review}</small>
