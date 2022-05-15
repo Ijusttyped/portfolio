3fe73c31217d0df9e6ac2a5510f60313f9a51820
Marcel Fernandez Rosas <60543555+Ijusttyped@users.noreply.github.com>
2022-05-15 15:39:08 +0200

    Corrected link in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,16 +7,16 @@ import {HashLink as Link} from "react-router-hash-link";
 const Footer = () => {
     return (
         <footer>
-            <a href={"/#"} className={"footer__logo"}>MFR</a>
+            <a className={"footer__logo"}>MFR</a>
 
             <ul className={"permalinks"}>
-                <li><Link to={{hash: "#"}}>Home</Link></li>
-                <li><Link to={{hash: "#about"}}>About</Link></li>
-                <li><Link to={{hash: "#experience"}}>Experience</Link></li>
+                <li><Link to="/#">Home</Link></li>
+                <li><Link to="/#about">About</Link></li>
+                <li><Link to="/#experience">Experience</Link></li>
                 {/*<li><a href={"#services"}>Services</a></li>*/}
-                <li><Link to={{hash: "#portfolio"}}>Portfolio</Link></li>
+                <li><Link to="/#portfolio">Portfolio</Link></li>
                 {/*<li><a href={"#testimonials"}>Testimonials</a></li>*/}
-                <li><Link to={{hash: "#contact"}}>Contact</Link></li>
+                <li><Link to="/#contact">Contact</Link></li>
             </ul>
 
             <div className={"footer__socials"}>
