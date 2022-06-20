3fe2d53de2b5042ae0d345775954a50cc6692d2c
Marcel Fernandez Rosas <60543555+Ijusttyped@users.noreply.github.com>
2022-06-20 18:34:03 +0200

    Added google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import ReactGA from "react-ga";
 import Home from "./components/home/Home";
 import CV from "./components/cv/CV";
 import Blog from "./components/blog/Blog";
 import NoMatch from "./components/nomatch/NoMatch";
 
+const TRACKING_ID = "UA-232451269-1";
+ReactGA.initialize(TRACKING_ID);
+
 const App = () => {
+
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
 
     return (
         <>
